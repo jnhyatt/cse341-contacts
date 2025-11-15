31365985de43e41edea6a9a2802bd26da947e09a
JoshValjosh <joshua.n.hyatt@gmail.com>
2025-11-14 20:13:33 -0700

    Generate swagger.json

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+import fs from "fs";
 import express from "express";
 import cors from "cors";
 import contactsRouter from "./routes/contacts.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpecs from "./docs/swagger.js";
+
+// Write swaggerSpecs to a JSON file. I set up swagger-jsdoc before I realized the assignment needs
+// a static JSON file for the OpenAPI spec, but we're still going to use swagger-jsdoc to generate
+// it for us so we don't have to write it by hand.
+fs.writeFileSync("swagger.json", JSON.stringify(swaggerSpecs, null, 2));
 
 const app = express();
 app.use(cors());
